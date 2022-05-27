315bbe31497b147c1f14769ec4ab3cdb3b4b2a41
gaabrielcamargo <gcamargo.dev@gmail.com>
2022-05-27 00:05:28 -0300

    feat(task): created task body and its styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
